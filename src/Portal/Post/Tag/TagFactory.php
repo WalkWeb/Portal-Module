597fe59cd3985275c6ab5f988e86fd5451e9897c
WalkWeb <av18775@list.ru>
2022-09-24 22:35:58 +0300

    Added TagFactory

---
+++ src/Portal/Post/Tag/TagFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Portal\Post\Tag;
+
+use Portal\Traits\Validation\ValidationException;
+use Portal\Traits\Validation\ValidationTrait;
+
+class TagFactory
+{
+    use ValidationTrait;
+
+    /**
+     * Создает тег на основе массива с данными
+     *
+     * @param array $data
+     * @return TagInterface
+     * @throws ValidationException
+     */
+    public function create(array $data): TagInterface
+    {
+        return new Tag(
+            self::string($data, 'id', TagException::INVALID_ID),
+            self::string($data, 'name', TagException::INVALID_NAME),
+            self::string($data, 'slug', TagException::INVALID_SLUG),
+            self::string($data, 'icon', TagException::INVALID_ICON),
+            self::string($data, 'preview_post_id', TagException::INVALID_PREVIEW_POST_ID),
+            self::bool($data, 'approved', TagException::INVALID_APPROVED)
+        );
+    }
+}
